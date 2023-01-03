8c0c061bedc799d3a88c42ead69b26ec662daecd
Nahiyan16 <nahiyanahmed11@gmail.com>
2023-01-02 19:07:27 -0500

    1

--- quizzal/src/Components/Answer.jsx
+++ quizzal/src/Components/Answer.jsx
@@ -1,17 +1,32 @@
 import React from "react"
 
 export default function Answer(props){
+    function getQuestions(){
+        let ary = []
+        let index = 0
+        let randNum = Math.floor(Math.random() * 4) + 1
+        for(let i = 0; i < 4; i++){
+            if(i === randNum){
+                ary.push(props.answer.correctAnswer)
+            }
+            else{
+                ary.push(props.answer.incorrectAnswers[index])
+                index++
+            }
+        }
 
-    
+        return (
+        <div className='answerList'>
+            <p>{ary[0]}</p>
+            <p>{ary[1]}</p>
+            <p>{ary[2]}</p>
+            <p>{ary[3]}</p>
+        </div>)
+    }
 
     return (
         <div>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
+            {getQuestions()}
         </div>
     )
 }
